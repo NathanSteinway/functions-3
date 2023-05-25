2f780bff2b8c561495d939b054e5b7aa159efd53
ganbari <steinway.nathan@gmail.com>
2023-05-25 15:37:26 -0500

    completed arrayMethodsTwo

--- arrayMethodsTwo.js
+++ arrayMethodsTwo.js
@@ -9,8 +9,9 @@ let numbers = [40, 68, 14, 90, 49];
     Use forEach method to loop though all the elements in the 'numbers' array above, and divide each element (number) by 2. Print the result in console.
 */
 
-// CODE HERE
-
+let newNumbers = numbers.forEach((element => {
+  console.log(element / 2)
+}))
 
 ////////// PROBLEM 2 //////////
 
@@ -22,7 +23,11 @@ let numbers = [40, 68, 14, 90, 49];
     NOTE: make sure you save the code in a variable, then console.log the variable to see the new array.
 */
 
-// CODE HERE
+let newArray = numbers.map((element => {
+  return element / 2
+}))
+
+console.log(newArray)
 
 
 ////////// PROBLEM 3 //////////
@@ -49,7 +54,11 @@ let cities = [
     Make sure you save the code in a variable. Then, console.log the variable to see the new array.
 */
 
-// CODE HERE
+let newCities = cities.filter((element => {
+  return element.charAt(element.length - 1) == 'i'
+}))
+
+console.log(newCities)
 
 
 ////////// PROBLEM 4 //////////
@@ -85,9 +94,15 @@ let fruits = [
     Note: do a google search on how to find an element's index of an array in Javascript. There is a built-in Javascript method that would help you find an index of an element in an array.
 */
 
-// CODE HERE
-
-
+let newFruit = fruits.forEach((element => {
+  if(element.color === "red"){
+    console.log("The fruit with an index of " + fruits.indexOf(element) + " is an apple")
+  } else if(element.color === "orange"){
+    console.log("The fruit with an index " + fruits.indexOf(element) + " is an orange.")
+  } else {
+    console.log("The fruit with an index " + fruits.indexOf(element) + " is neither apple or orange.")
+  }
+}))
 
 /* 
     Run the code in node to see the result. Then, copy and paste the result below.
@@ -101,6 +116,10 @@ let fruits = [
 
 // COPY AND PASTE THE RESULT FROM YOUR CONSOLE (NODE CONSOLE IS YOUR TERMINAL) HERE (AS A MULTI LINES COMMENT)
 
+// The fruit with an index 0 is neither apple or orange.
+// The fruit with an index 1 is an orange.
+// The fruit with an index 2 is neither apple or orange.
+// The fruit with an index of 3 is an apple
 
 
 ////////// PROBLEM 5 //////////
@@ -144,8 +163,7 @@ let foods = [
     Count the total price of foods that have "rice" tag. Please type the result too.
 */
 
-// CODE HERE
+let newFoods = foods.filter((element) => element.tags.includes("rice")).reduce((acc, curr) => acc + curr.price, 0)
 
-
-// THE TOTAL
+console.log(newFoods)
 
